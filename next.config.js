3b0ea53ad96fd37ab234d1af1905c4c6e070fad9
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-06-04 17:10:08 +0900

    feat: console 제거 룰 추가

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   compiler: {
     // / Enabled by default in development, disabled in production to reduce file size,
       // setting this will override the default for all environments.
-    styledComponents: true
+    styledComponents: true,
+    removeConsole: {
+      // Remove console.* output except console.error:
+      exclude: ['error'],
+    },
   }
 
 };
