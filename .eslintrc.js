c9e13c33ddd7de78a8698c8c9b25d282e217f973
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-06-04 16:48:20 +0900

    feat: lint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: 'next/core-web-vitals',
+  rules: {
+    'no-unused-expressions': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    'function-paren-newline': ['error', 'multiline'],
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+  }
+};
