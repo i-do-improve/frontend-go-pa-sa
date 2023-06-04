aaeb3626e8c6d63ba5c48120e71e9c945978277e
(FE) Noopy_jeongs <kimm.jeongs@gmail.com>
2023-06-04 17:14:32 +0900

    chore: 패키지 기초 세팅 (#10)
    * feat: lint
    
    * feat: 필요없는 공백 삭제 룰 추가
    
    * feat: layout에 StyledComponentsRegistry 추가
    
    * feat: styledComponent 기초 세팅 끝
    
    * feat: 안쓰는 css 파일 삭제
    
    * feat: console 제거 룰 추가
    
    * feat: jest.config.js 기초 세팅 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+  compiler: {
+    // / Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+    styledComponents: true,
+    removeConsole: {
+      // Remove console.* output except console.error:
+      exclude: ['error'],
+    },
+  }
 
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
