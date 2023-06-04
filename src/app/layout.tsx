8a4e0f010927f46d4c6b6f250ef5f6c5fe7ee03d
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-06-04 16:59:22 +0900

    feat: layout에 StyledComponentsRegistry 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import StyledComponentsRegistry from './lib/registry';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,8 +13,8 @@ export default function RootLayout({ children }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko">
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
     </html>
   );
 }
