8a03e20cba454e551dfd629c6d1a4ea85df58648
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-06-08 21:28:36 +0900

    feat: layout 공통 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,43 @@
 'use client';
-import { NavigationBar } from './components';
-import { Roboto } from 'next/font/google';
+import { NavigationBar, Header } from './components';
+import { Roboto, Poppins } from 'next/font/google';
+import './globals.css';
+import styled from 'styled-components';
 
 const roboto = Roboto({
   weight: ['400', '500', '700'],
   style: ['normal'],
   subsets: ['latin'],
+  variable: '--font-roboto',
   display: 'swap',
 });
+
+const poppins = Poppins({
+  weight: ['700', '900'],
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-poppins',
+});
+
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="ko" className={roboto.className}>
+    <html lang="ko" className={`${roboto.variable} ${poppins.variable}`}>
       <body>
-        {children}
+        <Header />
+        <Main>
+          {children}
+        </Main>
         <NavigationBar />
       </body>
     </html>
   );
 }
+
+const Main = styled.main`
+  height: calc(100vh - 78px - 44px);
+`;
