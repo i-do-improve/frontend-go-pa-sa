4ddac587d465353eb58846e07ae4647695d3deeb
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-06-08 20:40:55 +0900

    feat: 홈 링크 적용

--- src/app/components/NavigationBar/NavigationBar.tsx
+++ src/app/components/NavigationBar/NavigationBar.tsx
@@ -1,14 +1,47 @@
+import styled from 'styled-components';
 import { HomeIcon } from '../Icon';
+import NextLink from 'next/link';
 
 function NavigationBar() {
   return (
-    <div>
-      <HomeIcon />
+    <Ul>
+      <Link href="/">
+        <Li>
+          <HomeIcon />
+          홈
+        </Li>
+      </Link>
+      <Link href="/">
+        <Li>
+          <HomeIcon />
+          카테고리
+        </Li>
+      </Link>
       <h1>카테고리</h1>
       <h1>검색</h1>
       <h1>로그인/가입</h1>
-    </div>
+    </Ul>
   );
 }
+
+const Ul = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  padding: none;
+  margin: none;
+`;
+
+const Link = styled(NextLink)`
+  text-decoration: none;
+  color: inherit
+`;
+
+const Li = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 11px;
+`;
 
 export default NavigationBar;
