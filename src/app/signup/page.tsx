3c57613e1f2a3b20f263ab66f08ae18f36a8ae5d
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-08-31 21:35:09 +0900

    feat: Header enableBack 기능 추가

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { Header } from '../components';
+
+function SignupPage() {
+  return (
+    <div>
+      <Header enabledLogo={false} enabledBack />
+      <h1>Signup Page</h1>
+    </div>
+  );
+}
+
+export default SignupPage;
